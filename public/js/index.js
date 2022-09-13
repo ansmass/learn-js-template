c0d054a61b7f49dd0d5ca99788719435ad3fd9a8
Anselme <petit.singevagabondeur@gmail.com>
2022-09-13 09:45:10 +0200

    values are show with avatar, first and last name and email

--- public/js/index.js
+++ public/js/index.js
@@ -15,27 +15,33 @@ async function getRandomUsers() {
  */
 function showValue(value){
     let tbody = document.getElementById('table_body');
-    
-    for(var i = 0; i < value.length; i++){
+
+    value.forEach(user => {
         let tr = document.createElement('tr');
-        let td = document.createElement('td');
-        let avatar = document.createElement('img');
-        let firstName = document.createElement('p');
-        let lastName = document.createElement('p');
-        let email = document.createElement('p');
         
-        td.appendChild(avatar);
-        td.appendChild(firstName);
-        td.appendChild(lastName);
-        td.appendChild(email);
-
-        tr.appendChild(td);
-
+        let avatar = document.createElement('td');
+        let avatarImg = document.createElement('img');
+        avatarImg.src = user.avatar;
+        avatar.appendChild(avatarImg);
+        tr.appendChild(avatar);
+        
+        let lastName = document.createElement('td');
+        lastName.innerHTML = user.last_name;
+        tr.appendChild(lastName);
+        
+        let firstName = document.createElement('td');
+        firstName.innerHTML = user.first_name;
+        tr.appendChild(firstName);
+        
+        let email = document.createElement('td');
+        email.innerHTML = user.email;
+        tr.appendChild(email);
+        
         tbody.appendChild(tr);
-    }
+    });
 }
 
 function init() {
-    getRandomUsers().then((value) => {console.log(value['avatar'] + value['last_name'] + value['first_name'] + value['email']);}, (error) => console.error(error));   
+    getRandomUsers().then((value) => {showValue(value);}, (error) => console.error(error));   
 }
 init();
