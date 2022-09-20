4a337d7efdf584bd23da330dcb96213faeecba95
Anselme <petit.singevagabondeur@gmail.com>
2022-09-20 14:36:28 +0200

    refactor: re use method to create user

--- public/js/exercices/2/types/Table.js
+++ public/js/exercices/2/types/Table.js
@@ -28,7 +28,7 @@ export class Table extends User {
     /**
      * @description Delete and recharge data of table
      */
-    async refereshData(){
+    async refereshData() {
         await this.getRandomUsers();
         this.fillTable();
     }
@@ -38,64 +38,14 @@ export class Table extends User {
      */
     fillTable() {
         this.users.forEach(user => {
-            let tr = document.createElement('tr');
-            tr.id = user.uid;
-
-            // Table data to avatar
-            let userAvatarTd = document.createElement('td');
-            let userAvatar = document.createElement('img');
-            userAvatar.src = user.avatar;
-            userAvatarTd.appendChild(userAvatar);
-            tr.appendChild(userAvatarTd);
-
-            // Table data to last name
-            let userLastName = document.createElement('td')
-            userLastName.innerText = user.last_name;
-            tr.appendChild(userLastName)
-
-            // Table data to first name
-            let userFirstName = document.createElement('td');
-            userFirstName.innerText = user.first_name;
-            tr.appendChild(userFirstName);
-
-            // Table data to email
-            let userEmail = document.createElement('td');
-            userEmail.innerText = user.email;
-            tr.appendChild(userEmail);
-
-            // Table data to actions button
-            let button = document.createElement('td');
-            tr.appendChild(button);
-
-            // Button to delete row one by one of the table
-            const deleteButton = document.createElement('button');
-            button.appendChild(deleteButton);
-            deleteButton.innerText = 'Delete';
-            deleteButton.addEventListener('click', () => {
-                tr.remove(tr.children);
-            });
-
-            // Button to update data of user
-            const updateButton = document.createElement('button');
-            button.appendChild(updateButton);
-            updateButton.innerText = 'Update';
-            updateButton.addEventListener('click', () => {
-                this.modal.style.display = 'flex';
-                this.submitAddButton.style.display = 'none';
-                this.submitUpdateButton.style.display = 'block';
-
-                this.fillModalForm(tr);
-                this.userIdToUpdate = tr.id;
-            });
-
-            this.tbody.appendChild(tr);
+            this.creatRowTable(user);
         });
     }
 
     /**
      * @description All interaction with the buttons
      */
-    addEventOnButtons(){
+    addEventOnButtons() {
         const closeBtnModal = document.getElementById('close-cross');
         const refreshButton = document.getElementById('container_refresh-button');
         const addNewUserButton = document.getElementById('container_add-user-button');
@@ -112,9 +62,9 @@ export class Table extends User {
         submitUpdateButton.addEventListener('click', () => {
             const trToUpdate = document.getElementById(this.userIdToUpdate);
 
-            trToUpdate.children[3].innerText = this.inputUserEmail.value; 
-            trToUpdate.children[1].innerText = this.inputUserLastName.value; 
-            trToUpdate.children[2].innerText = this.inputUserFirstName.value; 
+            trToUpdate.children[3].innerText = this.inputUserEmail.value;
+            trToUpdate.children[1].innerText = this.inputUserLastName.value;
+            trToUpdate.children[2].innerText = this.inputUserFirstName.value;
             trToUpdate.children[0].firstChild.src = this.inputUserAvatar.value;
 
             this.modal.style.display = 'none';
@@ -128,8 +78,8 @@ export class Table extends User {
 
         submitAddButton.addEventListener('click', () => {
             this.modal.style.display = 'none';
-            this.addNewUser();
-        } )
+            this.creatRowTable();
+        })
     }
 
     /**
@@ -145,58 +95,85 @@ export class Table extends User {
      * @description fill the inputs of modal with data
      */
     fillModalForm(tr) {
-        this.inputUserEmail.value  = tr.children[3].innerText;
-        this.inputUserLastName.value  = tr.children[1].innerText;
-        this.inputUserFirstName.value  = tr.children[2].innerText;
-        this.inputUserAvatar.value  = tr.children[0].firstChild.src;
+        this.inputUserEmail.value = tr.children[3].innerText;
+        this.inputUserLastName.value = tr.children[1].innerText;
+        this.inputUserFirstName.value = tr.children[2].innerText;
+        this.inputUserAvatar.value = tr.children[0].firstChild.src;
     }
 
-    /**
-     * @description Creat new user
-     */
-    addNewUser(){
-        let tr = document.createElement('tr');
-        
-        // New user avatar
-        let newUserAvatarTd = document.createElement('td');
-        let newUserAvatar = document.createElement('img');
-        newUserAvatar.src = this.inputUserAvatar;
-        newUserAvatarTd.appendChild(newUserAvatar);
-        tr.appendChild(newUserAvatarTd);
-
-        // New user last name
-        let newUserLastName = document.createElement('td');
-        newUserLastName.innerText = this.inputUserLastName;
-        tr.appendChild(newUserLastName);
-
-        // New user first name
-        let newUserFirstName = document.createElement('td');
-        newUserFirstName.innerText = this.inputUserFirstName;
-        tr.appendChild(newUserFirstName);
-
-        // New user email
-        let newUserEmail = document.createElement('td');
-        newUserEmail.innerText = this.inputUserEmail;
-        tr.appendChild(newUserEmail);
-        
+    creatRowTable(user) {
+        const tr = document.createElement('tr');
+        const userAvatarTd = document.createElement('td');
+        const userAvatar = document.createElement('img');
+        const userLastName = document.createElement('td')
+        const userFirstName = document.createElement('td');
+        const userEmail = document.createElement('td');
         let button = document.createElement('td');
-        tr.appendChild(button);
-
         const deleteButton = document.createElement('button');
+        const updateButton = document.createElement('button');
+
+        if (user === null || user === undefined) {
+            // Set avatar
+            userAvatar.src = this.inputUserAvatar.value;
+            userAvatarTd.appendChild(userAvatar);
+            tr.appendChild(userAvatarTd);
+
+            userLastName.innerText = this.inputUserLastName.value;
+            tr.appendChild(userLastName);
+
+            userFirstName.innerText = this.inputUserFirstName.value;
+            tr.appendChild(userFirstName);
+
+            userEmail.innerText = this.inputUserEmail.value;
+            tr.appendChild(userEmail);
+
+            tr.appendChild(button);
+        } else {
+            tr.id = user.uid;
+
+            // Table data to avatar
+            userAvatar.src = user.avatar;
+            userAvatarTd.appendChild(userAvatar);
+            tr.appendChild(userAvatarTd);
+
+            // Table data to last name
+
+            userLastName.innerText = user.last_name;
+            tr.appendChild(userLastName)
+
+            // Table data to first name
+            userFirstName.innerText = user.first_name;
+            tr.appendChild(userFirstName);
+
+            // Table data to email
+            userEmail.innerText = user.email;
+            tr.appendChild(userEmail);
+
+            // Table data to actions button
+            tr.appendChild(button);
+        }
+
+        // Button to delete row one by one of the table
         button.appendChild(deleteButton);
         deleteButton.innerText = 'Delete';
         deleteButton.addEventListener('click', () => {
             tr.remove(tr.children);
         });
 
-        const updateButton = document.createElement('button');
+        // Button to update data of user
         button.appendChild(updateButton);
         updateButton.innerText = 'Update';
         updateButton.addEventListener('click', () => {
             this.modal.style.display = 'flex';
+            this.submitAddButton.style.display = 'none';
+            this.submitUpdateButton.style.display = 'block';
+
+            if (user !== null || user !== undefined) {
+                this.fillModalForm(tr);
+                this.userIdToUpdate = tr.id;
+            }
         });
 
         this.tbody.appendChild(tr);
     }
-
 }
